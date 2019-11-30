cc9ad2be0e295dc41a24ff0dcdba1c2f2f79bb05
alexizyd <56993198+alexizyd@users.noreply.github.com>
2019-11-30 11:54:44 +0100

    weather search with coordinates

--- src/js/apiController.js
+++ src/js/apiController.js
@@ -4,8 +4,15 @@ var weatherClient = require('./weather_client.js');
 async function runApiClients(city) {
 	airPollutionClient.getAirPollution(city.lat, city.lng)
 		.then(pollutionData => airPollutionClient.setAirPollution(pollutionData));
-	weatherClient.forecastWeather(city.city)
+	weatherClient.forecastWeather(city.lat, city.lng)
 	.then(resultFromServer => weatherClient.setWeather(resultFromServer));
+	console.log(city);
+
+	document.querySelector('.city-name-rel h1').innerText = city.city;
+	document.querySelector('.city-name-rel p').innerText = city.country;
+
+	document.getElementById('city-country').innerText = city.city;
+    document.getElementById('country').innerText = city.country;
 }
 
 module.exports.runApiClients = runApiClients;
